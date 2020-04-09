f59834e422f7cf128fc0bf0bd3943b824214ee65
andres22atzel <andres.atzel@gmail.com>
2020-04-09 16:54:37 -0500

    creation of new component and nesting inside main App component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//This is our root component, is the first component to look at moment of launch
+import React from "react";
+//Here we import the Nijas componet to the main App component
+import Ninjas from "./Ninjas";
 
+//here we are using a functional component instead of a class component
+//In this case the render function is not needed
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First React app!</h1>
+      <p>Welcom :)</p>
+      {/*Here we are nesting the Ninjas component as a tag*/}
+      <Ninjas />
     </div>
   );
 }
