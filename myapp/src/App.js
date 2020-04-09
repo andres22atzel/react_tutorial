122a8882596792a6f10e30b145c27fdf88f2f9c4
andres22atzel <andres.atzel@gmail.com>
2020-04-09 17:20:16 -0500

    Using props and destructuring, algo how to recycle components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,8 +10,13 @@ function App() {
     <div className="App">
       <h1>My First React app!</h1>
       <p>Welcom :)</p>
-      {/*Here we are nesting the Ninjas component as a tag*/}
-      <Ninjas />
+      {/*Here we are nesting the Ninjas component as a tag
+        Also we are sending object info through props, this sintax
+        is for functional components, for class components is different.
+      */}
+      <Ninjas name={'Ryu'} age={25} belt={'Black'}/>
+      {/**here we are recycling the Ninjas component */}
+      <Ninjas name={'Yoshi'} age={80} belt={'White'}/>
     </div>
   );
 }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Ninjas() {
+//Here we see the use of props, the props info is sended from the main App component
+//To the Ninjas functional component through parameter and then use through the code
+//props contain the props object info, it can be name different
+function Ninjas(props) {
+  // console.log(props);
+  //Here we are using destructuring, we are creating three variables from the info in our props
+  const { name, age, belt } = props;
   return (
     <div className="ninja">
-      <div>Name: Ryu</div>
-      <div>Age: 30</div>
-      <div>Belt: Black</div>
+      {/*Here we are using destructuring variables*/}
+      <div>Name: {name}</div>
+      {/*Here we are using the props directly*/}
+      <div>Age: {props.age}</div>
+      <div>Belt: {props.belt}</div>
     </div>
   );
 }
