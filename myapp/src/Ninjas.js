985ab3fc3f43a8603e0db10178859464ebfbe887
andres22atzel <andres.atzel@gmail.com>
2020-04-10 13:13:37 -0500

    Conditionals and ternary operator

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,23 +3,36 @@ import React from "react";
 //Here we see the use of props, the props info is sended from the main App component
 //To the Ninjas functional component through parameter and then use through the code
 //props contain the props object info, it can be name different
-function Ninjas(props) {
+//With funtional components, we can destructure the props directly in the function parameter parentesis
+function Ninjas({ ninjas }) {
   // console.log(props);
-  //Here we are using destructuring, we are creating three variables from the info in our props
-  const { ninjas } = props;
   const ninjaList = ninjas.map((ninja) => {
-    return (
-      //Is important to consider placing a unique key for every single object in the list
-      //In this case we are using the id the every object has in the object, by we can use
-      //The index of the array
+    //here we are using conditionals
+    // if (ninja.age > 20) {
+    //   return (
+    //     //Is important to consider placing a unique key for every single object in the list
+    //     //In this case we are using the id the every object has in the object, by we can use
+    //     //The index of the array
+    //     <div className="ninja" key={ninja.id}>
+    //       {/*Here we are using destructuring variables*/}
+    //       <div>Name: {ninja.name}</div>
+    //       {/*Here we are using the props directly*/}
+    //       <div>Age: {ninja.age}</div>
+    //       <div>Belt: {ninja.belt}</div>
+    //     </div>
+    //   );
+    // } else {
+    //   return null;
+    // }
+    // // here we are using ternary operator for condiciotinals
+    // // Sintax is firt condition ? what to display if its true : what to display if its false
+    return ninja.age > 20 ? (
       <div className="ninja" key={ninja.id}>
-        {/*Here we are using destructuring variables*/}
         <div>Name: {ninja.name}</div>
-        {/*Here we are using the props directly*/}
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
       </div>
-    );
+    ) : null;
   });
 
   return (
